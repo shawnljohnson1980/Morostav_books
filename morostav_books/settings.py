cf46b44b33f1bf03899347bae30d102765bdb810
shawnljohnson1980 <shawnljohnson1980@yahoo.com>
2025-03-19 13:29:44 -0400

    site side models and pages updates and enhancements.

--- morostav_books/settings.py
+++ morostav_books/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'morostav_books.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'm_b.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'm_b',
+        'USER': 'morostav_user',
+        'PASSWORD': 'Slj@Kmj@0612$$%%',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
